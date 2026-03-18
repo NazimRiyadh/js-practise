b06ae3b93e63f168c81324eec8f891c61ca44b9f
NazimRiyadh <neariyadh786@gmail.com>
2026-03-18 22:13:04 +0600

    Dom selector

--- Async_js/async_await.js
+++ Async_js/async_await.js
@@ -1,40 +1,40 @@
 function check_order() {
     return new Promise((resolve) => {
-        setTimeout(()=>{
+        setTimeout(() => {
             console.log("Checking order...")
             resolve()
-        },1000)
+        }, 1000)
     })
 }
 
 function create_order() {
     return new Promise((resolve) => {
-        setTimeout(()=>{
+        setTimeout(() => {
             console.log("Order created")
             resolve()
-        },1000)
+        }, 1000)
     })
 }
 
 function charge_payment() {
-    return new Promise((resolve)=>{
-    setTimeout(()=>{  
-        console.log("Payment charged")
-        resolve()
-        },1000)
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Payment charged")
+            resolve()
+        }, 1000)
     })
 }
 
 function send_invoice() {
     return new Promise((resolve) => {
-    setTimeout(() => {
+        setTimeout(() => {
             console.log("Invoice sent")
-        resolve() 
+            resolve()
         }, 1000);
     })
 }
 
-async function main(){
+async function main() {
     await check_order()
     await create_order()
     await charge_payment()
